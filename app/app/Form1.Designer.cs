183fff5c998a705a9ca4e83f518a98059918579c
Simon Zhao <zzhao901@gmail.com>
2018-05-13 00:17:08 +0800

    汇总计算器调整窗口大小

--- app/app/Form1.Designer.cs
+++ app/app/Form1.Designer.cs
@@ -33,39 +33,48 @@
             this.inputText = new System.Windows.Forms.RichTextBox();
             this.submit = new System.Windows.Forms.Button();
             this.clearAll = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.clearResult = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // resultText
             // 
+            this.resultText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.resultText.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.resultText.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.resultText.Location = new System.Drawing.Point(12, 27);
+            this.resultText.Location = new System.Drawing.Point(0, 22);
             this.resultText.Margin = new System.Windows.Forms.Padding(20, 0, 0, 0);
             this.resultText.Name = "resultText";
             this.resultText.ReadOnly = true;
-            this.resultText.Size = new System.Drawing.Size(463, 145);
+            this.resultText.Size = new System.Drawing.Size(454, 111);
             this.resultText.TabIndex = 0;
             this.resultText.Text = "";
             this.resultText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.resultText_MouseDown);
             // 
             // inputText
             // 
+            this.inputText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.inputText.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.inputText.Location = new System.Drawing.Point(12, 209);
+            this.inputText.Location = new System.Drawing.Point(3, 17);
             this.inputText.Margin = new System.Windows.Forms.Padding(0);
             this.inputText.Name = "inputText";
             this.inputText.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.inputText.Size = new System.Drawing.Size(463, 189);
+            this.inputText.Size = new System.Drawing.Size(450, 189);
             this.inputText.TabIndex = 1;
             this.inputText.Text = "";
             this.inputText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.inputText_MouseDown);
             // 
             // submit
             // 
-            this.submit.Location = new System.Drawing.Point(400, 414);
+            this.submit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.submit.Location = new System.Drawing.Point(375, 214);
             this.submit.Name = "submit";
             this.submit.Size = new System.Drawing.Size(75, 23);
             this.submit.TabIndex = 2;
@@ -75,64 +84,89 @@
             // 
             // clearAll
             // 
-            this.clearAll.Location = new System.Drawing.Point(110, 414);
+            this.clearAll.Location = new System.Drawing.Point(109, 214);
             this.clearAll.Name = "clearAll";
-            this.clearAll.Size = new System.Drawing.Size(75, 23);
+            this.clearAll.Size = new System.Drawing.Size(85, 23);
             this.clearAll.TabIndex = 3;
             this.clearAll.Text = "清空全部";
             this.clearAll.UseVisualStyleBackColor = true;
             this.clearAll.Click += new System.EventHandler(this.clear_button_click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 14);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "汇总结果：";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(12, 187);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 14);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "请输入数据：";
-            // 
             // clearResult
             // 
-            this.clearResult.Location = new System.Drawing.Point(12, 414);
+            this.clearResult.Location = new System.Drawing.Point(3, 214);
             this.clearResult.Name = "clearResult";
-            this.clearResult.Size = new System.Drawing.Size(75, 23);
+            this.clearResult.Size = new System.Drawing.Size(86, 23);
             this.clearResult.TabIndex = 6;
             this.clearResult.Text = "清空结果";
             this.clearResult.UseVisualStyleBackColor = true;
             this.clearResult.Click += new System.EventHandler(this.clearResult_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Size = new System.Drawing.Size(463, 423);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.inputText);
+            this.groupBox3.Controls.Add(this.clearResult);
+            this.groupBox3.Controls.Add(this.submit);
+            this.groupBox3.Controls.Add(this.clearAll);
+            this.groupBox3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(4, 169);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(456, 245);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "请输入数据";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.resultText);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(3, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(457, 136);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "汇总结果";
+            // 
             // WinForm
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 444);
-            this.Controls.Add(this.clearResult);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.clearAll);
-            this.Controls.Add(this.submit);
-            this.Controls.Add(this.inputText);
-            this.Controls.Add(this.resultText);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(487, 447);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.MinimumSize = new System.Drawing.Size(487, 447);
             this.Name = "WinForm";
             this.Text = "汇总计算器";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -158,9 +192,10 @@
         private System.Windows.Forms.Button submit;
         private System.Windows.Forms.Button clearAll;
         private System.Collections.Generic.Dictionary<string, int> dictionary;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button clearResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 
 
--- app/app/Form1.cs
+++ app/app/Form1.cs
@@ -8,22 +8,200 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Runtime.InteropServices;
 
 namespace app
 {
     public partial class WinForm : Form
     {
+
+        #region 窗体边框阴影动画效果移动改变大小
+        const int CS_DropSHADOW = 0x20000;
+        const int GCL_STYLE = (-26);
+        //声明Win32 API
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SetClassLong(IntPtr hwnd, int nIndex, int dwNewLong);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetClassLong(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32")]
+        private static extern bool AnimateWindow(IntPtr hwnd, int dwTime, int dwFlags);
+
+        /*
+
+         * 函数功能：该函数能在显示与隐藏窗口时能产生特殊的效果。有两种类型的动画效果：滚动动画和滑动动画。
+
+         * 函数原型：BOOL AnimateWindow（HWND hWnd，DWORD dwTime，DWORD dwFlags）；
+
+         * hWnd：指定产生动画的窗口的句柄。
+
+         * dwTime：指明动画持续的时间（以微秒计），完成一个动画的标准时间为200微秒。
+
+         * dwFags：指定动画类型。这个参数可以是一个或多个下列标志的组合。
+
+         * 返回值：如果函数成功，返回值为非零；如果函数失败，返回值为零。
+
+         * 在下列情况下函数将失败：窗口使用了窗口边界；窗口已经可见仍要显示窗口；窗口已经隐藏仍要隐藏窗口。若想获得更多错误信息，请调用GetLastError函数。
+
+         * 备注：可以将AW_HOR_POSITIVE或AW_HOR_NEGTVE与AW_VER_POSITVE或AW_VER_NEGATIVE组合来激活一个窗口。
+
+         * 可能需要在该窗口的窗口过程和它的子窗口的窗口过程中处理WM_PRINT或WM_PRINTCLIENT消息。对话框，控制，及共用控制已处理WM_PRINTCLIENT消息，缺省窗口过程也已处理WM_PRINT消息。
+
+         * 速查：WIDdOWS NT：5.0以上版本：Windows：98以上版本；Windows CE：不支持；头文件：Winuser.h；库文件：user32.lib。
+
+         */
+
+        //标志描述：
+
+        public const int AW_SLIDE = 0x40000;//使用滑动类型。缺省则为滚动动画类型。当使用AW_CENTER标志时，这个标志就被忽略。
+
+        public const int AW_ACTIVATE = 0x20000;//激活窗口。在使用了AW_HIDE标志后不要使用这个标志。
+
+        public const int AW_BLEND = 0x80000;//使用淡出效果。只有当hWnd为顶层窗口的时候才可以使用此标志。
+
+        public const int AW_HIDE = 0x10000;//隐藏窗口，缺省则显示窗口。(关闭窗口用)
+
+        public const int AW_CENTER = 0x0010;//若使用了AW_HIDE标志，则使窗口向内重叠；若未使用AW_HIDE标志，则使窗口向外扩展。
+
+        public const int AW_HOR_POSITIVE = 0x0001;//自左向右显示窗口。该标志可以在滚动动画和滑动动画中使用。当使用AW_CENTER标志时，该标志将被忽略。
+
+        public const int AW_VER_POSITIVE = 0x0004;//自顶向下显示窗口。该标志可以在滚动动画和滑动动画中使用。当使用AW_CENTER标志时，该标志将被忽略。
+
+        public const int AW_HOR_NEGATIVE = 0x0002;//自右向左显示窗口。该标志可以在滚动动画和滑动动画中使用。当使用AW_CENTER标志时，该标志将被忽略。
+
+        public const int AW_VER_NEGATIVE = 0x0008;//自下向上显示窗口。该标志可以在滚动动画和滑动动画中使用。当使用AW_CENTER标志时，该标志将被忽略。
+        //需添加using System.Runtime.InteropServices
+        [DllImport("user32.dll")]
+
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        //常量
+
+        public const int WM_SYSCOMMAND = 0x0112;
+
+        //窗体移动
+
+        public const int SC_MOVE = 0xF010;
+
+        public const int HTCAPTION = 0x0002;
+
+        //改变窗体大小
+        public const int WMSZ_LEFT = 0xF001;
+
+        public const int WMSZ_RIGHT = 0xF002;
+
+        public const int WMSZ_TOP = 0xF003;
+
+        public const int WMSZ_TOPLEFT = 0xF004;
+
+        public const int WMSZ_TOPRIGHT = 0xF005;
+
+        public const int WMSZ_BOTTOM = 0xF006;
+
+        public const int WMSZ_BOTTOMLEFT = 0xF007;
+
+        public const int WMSZ_BOTTOMRIGHT = 0xF008;
+        #endregion
+
         public WinForm()
         {
             InitializeComponent();
             this.initDictionay();
-           
+            SetClassLong(this.Handle, GCL_STYLE, GetClassLong(this.Handle, GCL_STYLE) | CS_DropSHADOW); //API函数加载，实现窗体边框阴影效果
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //窗体加载动画效果
+            //AnimateWindow(this.Handle, 500, AW_BLEND | AW_CENTER);
+        }
 
+        /// <summary>
+        /// 获取游标位置并改变形状
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = this.PointToClient(MousePosition);
+            int xx = Width;
+            int yy = Height;
+            //TopLeft
+            if (p.X <= 2 && p.Y <= 2)
+            {
+                this.Cursor = Cursors.SizeNWSE;
+                wParam = (new IntPtr(WMSZ_TOPLEFT)).ToInt32();
+            }
+            //TopRight
+            else if (p.X >= Width - 2 && p.Y <= 2)
+            {
+                this.Cursor = Cursors.SizeNESW;
+                wParam = (new IntPtr(WMSZ_TOPRIGHT)).ToInt32();
+            }
+            //BottomLeft
+            else if (p.X <= 2 && p.Y >= Height - 2)
+            {
+                this.Cursor = Cursors.SizeNESW;
+                wParam = (new IntPtr(WMSZ_BOTTOMLEFT)).ToInt32();
+            }
+            //BottomRight
+            else if (p.X >= Width - 2 && p.Y >= Height - 2)
+            {
+                this.Cursor = Cursors.SizeNWSE;
+                wParam = (new IntPtr(WMSZ_BOTTOMRIGHT)).ToInt32();
+            }
+            //Left
+            else if (p.Y > 2 && p.Y < Height - 2 && p.X < 2)
+            {
+                this.Cursor = Cursors.SizeWE;
+                wParam = (new IntPtr(WMSZ_LEFT)).ToInt32();
+            }
+            //Up
+            else if (p.X > 2 && p.X < Width - 2 && p.Y < 2)
+            {
+                this.Cursor = Cursors.SizeNS;
+                wParam = (new IntPtr(WMSZ_TOP)).ToInt32();
+            }
+            //Bottom
+            else if (p.X > 2 && p.X < Width - 2 && p.Y > Height - 2)
+            {
+                this.Cursor = Cursors.SizeNS;
+                wParam = (new IntPtr(WMSZ_BOTTOM)).ToInt32();
+            }
+            //Right
+            else if (p.Y > 2 && p.Y < Height - 2 && p.X > Width - 2)
+            {
+                this.Cursor = Cursors.SizeWE;
+                wParam = (new IntPtr(WMSZ_RIGHT)).ToInt32();
+            }
+            else
+                this.Cursor = Cursors.Default;
+
+
+        }
+
+        private int wParam = 0;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point p = this.PointToClient(MousePosition);
+            //Move
+            if (p.X > 2 && p.X < Width - 2 && p.Y > 2 && p.Y < Height - 2)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+            }
+            else // ChangeSize
+            {
+
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_SYSCOMMAND, wParam, IntPtr.Zero.ToInt32());
+
+            }
         }
 
         private void submit_button_click(object sender, EventArgs e)
@@ -76,7 +254,9 @@ namespace app
                             {
                                 itemDictionary[key] += value;
                             }
+                            break;
                         }
+                        
                     }
                     if(!isMatched)
                     {
@@ -283,5 +463,9 @@ namespace app
             textBox.Paste();//把剪贴板上的数据粘贴到目标RichTextBox
         }
 
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
